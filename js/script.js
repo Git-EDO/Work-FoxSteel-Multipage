cffd2a2392c43969818675bf418568b9b5964c01
Git-EDO <DemonEX@bk.ru>
2022-10-21 20:19:23 +0300

    goods

--- js/script.js
+++ js/script.js
@@ -97,3 +97,59 @@ footerScrollLink.addEventListener ('click', function(e) {
   const header = document.querySelector('.header')
   header.scrollIntoView({block: "start", behavior: "smooth"});
 })
+
+// Инпут цены
+
+const priceUp = document.querySelectorAll('.quantity-arrow-plus')
+const priceDown = document.querySelectorAll('.quantity-arrow-minus')
+
+priceUp.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const thisInput = btn.closest('.goods-filter-price-input')
+    thisInput.querySelector('input[type=number]').value++
+  })
+})
+
+priceDown.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const thisInput = btn.closest('.goods-filter-price-input')
+    if(thisInput.querySelector('input[type=number]').value > 1) {
+      thisInput.querySelector('input[type=number]').value--
+    }
+  })
+})
+
+// Открытие наименований
+
+const expandBtns = document.querySelectorAll('.goods-filter-show-btn')
+
+expandBtns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    btn.closest('.goods-filter-options').classList.add('active')
+    btn.style.display = 'none'
+  })
+})
+
+// Открытие aside
+
+const aside = document.querySelector('.goods-aside')
+const showAside = document.querySelector('.mobile-filters a')
+const closeFilters = document.querySelector('.goods-aside-close')
+
+showAside.addEventListener('click', (e) => {
+  e.preventDefault()
+  aside.classList.toggle('active')
+})
+
+document.addEventListener('click', (e) => {
+  if(!e.target.closest('.goods-aside') && !e.target.closest('.mobile-filters')) {
+    aside.classList.remove('active')
+  }
+})
+
+closeFilters.addEventListener('click', () => {
+  aside.classList.remove('active')
+})
